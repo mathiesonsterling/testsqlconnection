8982d4336a13e22938d9d12470b76a48bfa4fdce
Mathieson Sterling <gsterling@apprenda.com>
2017-11-28 08:58:33 -0500

    Added code used to make easy test program

---
+++ testsqlserverconnectionstring/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testsqlserverconnectionstring
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No argument for connection string provided!");
+                return;
+            }
+
+            var sqlda = new SqlDataAccess(args[0], Console.WriteLine);
+
+            var res = sqlda.Connect().Result;
+
+            Console.WriteLine(res ? "Connected to database" : "Could not connect to db");
+        }
+    }
+}
---
+++ testsqlserverconnectionstring/SqlDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testsqlserverconnectionstring
+{
+    class SqlDataAccess
+    {
+        private readonly string _connectionString;
+        private readonly Action<string> _log;
+
+        public SqlDataAccess(string connectionString, Action<string> logFunction)
+        {
+            _connectionString = connectionString;
+            _log = logFunction;
+        }
+
+        public async Task<bool> Connect()
+        {
+            var server = new SqlConnection(_connectionString);
+
+            try
+            {
+                await server.OpenAsync();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _log(e.Message);
+                return false;
+            }
+        }
+    }
+}
